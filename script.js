72f73764f3768076e44c17bcd4ebb0552e3b10ce
Sophie LAURENT <sophielaurent92@gmail.com>
2024-01-02 15:12:49 +0100

    First condition

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let score = 0
+
+let motUtilisateur = prompt("Entrez le mot : " + listeMots[0])
+
+if(motUtilisateur === listeMots[0]) {
+    score ++
+}
+console.log(score)
+
+motUtilisateur = prompt("Entrez le mot : " + listeMots[1])
+
+if(motUtilisateur === listeMots[1]) {
+    score ++
+}
+console.log(score)
+
+motUtilisateur = prompt("Entrez le mot : " + listeMots[2])
+
+if(motUtilisateur === listeMots[2]) {
+    score ++
+}
+console.log(score)
+
+console.log("Votre score final est de " + score + " sur 3")
+
